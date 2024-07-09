2863247ca6561c8677cb0a472f3355a750298d33
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2024-07-09 11:51:39 -0400

    Change conditional text

--- client/src/components/Tool/ToolSuccessMessage.vue
+++ client/src/components/Tool/ToolSuccessMessage.vue
@@ -3,17 +3,22 @@
         <p>
             Started tool <b>{{ toolName }}</b> and successfully added {{ nJobsText }} to the queue.
         </p>
-        <p>It produces {{ nOutputsText }}:</p>
-        <ul>
-            <li v-for="item of jobResponse.outputs" :key="item.hid">
-                <b>{{ item.hid }}: {{ item.name }}</b>
-            </li>
-        </ul>
-        <p>
-            You can check the status of queued jobs and view the resulting data by refreshing the History panel. When
-            the job has been run the status will change from 'running' to 'finished' if completed successfully or
-            'error' if problems were encountered.
+        <p v-if="nOutputs === 0">
+            Your job has no outputs. If you launched an Interactive Tool, the blue box above should update to provide a clickable link.
         </p>
+        <template v-else>
+            <p>It produces {{ nOutputsText }}:</p>
+            <ul>
+                <li v-for="item of jobResponse.outputs" :key="item.hid">
+                    <b>{{ item.hid }}: {{ item.name }}</b>
+                </li>
+            </ul>
+            <p>
+                You can check the status of queued jobs and view the resulting data by refreshing the History panel. When
+                the job has been run the status will change from 'running' to 'finished' if completed successfully or
+                'error' if problems were encountered.
+            </p>
+        </template>
     </div>
 </template>
 
